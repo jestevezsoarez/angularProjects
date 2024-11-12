07e7ed06b68b728d330e7f32da77a143b531879e
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-12 17:09:48 -0300

    separando en modulos la app

--- 03-giphy-system/src/app/gifs/gifs.module.ts
+++ 03-giphy-system/src/app/gifs/gifs.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchComponent } from './components/search/search.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
 @NgModule({
-    declarations: [],
+    declarations: [
+    HomePageComponent,
+    SearchComponent,
+    CardListComponent
+  ],
     imports: [
         CommonModule // Sirve para pipes, ngIf, ngFor
     ],
-    exports: [],
+    exports: [HomePageComponent],
     providers: [],
 })
 export class GifsModule { }
