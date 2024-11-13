0cc88c99f3bf391155466d46599bcb648f44ff8a
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-13 20:11:53 -0300

    trabajando en el servicio para ordenar los tags y mantenerlos en 10

--- 03-giphy-system/src/app/gifs/services/gifs.service.ts
+++ 03-giphy-system/src/app/gifs/services/gifs.service.ts
@@ -9,12 +9,26 @@ export class GifsService {
 
   constructor() { }
 
-  get tagsHistory(): string[] {
+  get tagsHistory() {
     return [...this._tagsHistory]; // como los arreglos pasan por referencia en js retorno una copia con el operador spread
   }
 
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {      
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+      console.log(this._tagsHistory);
+    }
+    this._tagsHistory = this._tagsHistory.splice(0, 9);
+    
+  }
+
   searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    this._tagsHistory.unshift(tag); // unsift agrega al inicio del array
   }
 
 }
