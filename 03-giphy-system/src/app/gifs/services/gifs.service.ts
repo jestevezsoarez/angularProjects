5e2a5e4bf1e5c234be8e329a4703769f823e6030
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-14 10:01:02 -0300

    hago peticiones http a la api de giphy con fetch

--- 03-giphy-system/src/app/gifs/services/gifs.service.ts
+++ 03-giphy-system/src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root' // root para que sea global el servicio
+  providedIn: 'root', // root para que sea global el servicio
 })
 export class GifsService {
-
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'uwiy2NxdVdsk3m5DGWgvUCIXNKxVCBf3';
 
-  constructor() { }
+  constructor() {}
 
   get tagsHistory() {
     return [...this._tagsHistory]; // como los arreglos pasan por referencia en js retorno una copia con el operador spread
@@ -16,19 +16,20 @@ export class GifsService {
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if (this._tagsHistory.includes(tag)) {      
+    if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
-      console.log(this._tagsHistory);
+      this._tagsHistory.unshift(tag); // unsift agrega al inicio del array
+      this._tagsHistory = this._tagsHistory.splice(0, 9);
     }
-    this._tagsHistory = this._tagsHistory.splice(0, 9);
-    
   }
 
-  searchTag(tag: string): void {
+  // esta es una forma de hacer peticiones http con fetch
+  async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
-
-    this._tagsHistory.unshift(tag); // unsift agrega al inicio del array
+    // el fetch regresa una promesa
+    fetch('https://api.giphy.com/v1/stickers/search?api_key=uwiy2NxdVdsk3m5DGWgvUCIXNKxVCBf3&q=superman&limit=20')
+      .then(resp => resp.json()) 
+      .then (data => console.log(data));
   }
-
 }
