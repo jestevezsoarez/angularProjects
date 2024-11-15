76f8b07015ed533e72c45586b05e6f1ee4d50713
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-15 20:59:17 -0300

    componente personalizado para lazy image

--- 03-giphy-system/src/app/gifs/components/card/card.component.ts
+++ 03-giphy-system/src/app/gifs/components/card/card.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card',
   templateUrl: './card.component.html'
 })
-export class CardComponent {
-
+export class CardComponent implements OnInit {
+  
   @Input() gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required'); // Validacion. gif es una propiedad requerida.
+  }
 }
--- 03-giphy-system/src/app/gifs/gifs.module.ts
+++ 03-giphy-system/src/app/gifs/gifs.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+// Modules
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+// Components
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchComponent } from './components/search/search.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -14,7 +18,8 @@ import { CardComponent } from './components/card/card.component';
     CardComponent
   ],
     imports: [
-        CommonModule // Sirve para pipes, ngIf, ngFor
+        CommonModule, // Sirve para pipes, ngIf, ngFor
+        SharedModule
     ],
     exports: [HomePageComponent],
     providers: [],
