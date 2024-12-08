a25218d98ea20ad8e8bff8aea5041e782b7ab4e0
Javier Estevez <javierdaniloestevez@gmail.com>
2024-12-08 17:46:18 -0300

    project creation

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
-// Components
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule // El modulo Shared se carga de forma tradicional, el modulo Country se cargara de forma perezoza.
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 04-countryApp/src/app/countries/countries.module.ts
+++ 04-countryApp/src/app/countries/countries.module.ts
@@ -9,4 +9,4 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class CountryModule { }
+export class CountriesModule { }
