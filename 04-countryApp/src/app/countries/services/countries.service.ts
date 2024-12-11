58f4dcdae2bfafa3d3a8530a770390cd0979d3c1
Javier Estevez <javierdaniloestevez@gmail.com>
2024-12-11 19:48:38 -0300

    country interface and start fetching from api with rxjs observables

---
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({ providedIn: 'root' })
+export class CountryService {
+
+// Api -----> https://restcountries.com/
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) {}
+
+  searchByCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url);
+  }
+
+}
