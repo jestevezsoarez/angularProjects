4865d162d2f7b7453ba1d70ee3e6981e6a5bd61f
Javier Estevez <javierdaniloestevez@gmail.com>
2024-12-18 19:38:41 -0300

    Refactor service in fetch methods to reduce repetitive code

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -12,42 +12,41 @@ export class CountryService {
 
   constructor(private http: HttpClient) {}
 
+  // Private method to refactor repeated code in fetch methods
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url)
+      .pipe(
+        // Catch error and return error as an empty array with of operator
+        catchError( () => of([])),
+      );
+  }
+
   // https://restcountries.com/v3.1/alpha/{code}
   searchByAlphaCode(code: string): Observable<Country | null> { // return 1 country or null if it doesn't exist code
     const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url)
       .pipe(
         map(countries => countries.length > 0 ? countries[0] : null),
-        catchError(error => of(null))
+        catchError(() => of(null))
       );
   }
 
   // https://restcountries.com/v3.1/capital/{capital}
   searchByCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        // Catch error and return error as an empty array with of operator
-        catchError( error => of([]))
-      );
+    return this.getCountriesRequest(url);
   }
 
   // https://restcountries.com/v3.1/region/{region}
   searchByRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      );
+    return this.getCountriesRequest(url);
   }
 
   // https://restcountries.com/v3.1/name/{name}
   searchByCountryName(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      );
+    return this.getCountriesRequest(url);
   }
 
 }
