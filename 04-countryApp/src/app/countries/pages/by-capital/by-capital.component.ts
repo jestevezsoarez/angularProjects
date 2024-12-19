75fb1ac4a71f7c2a66b7f9f98922c541a12ae809
Javier Estevez <javierdaniloestevez@gmail.com>
2024-12-19 18:26:39 -0300

    added a spinner to let the user know that page is loading

--- 04-countryApp/src/app/countries/pages/by-capital/by-capital.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital/by-capital.component.ts
@@ -12,13 +12,16 @@ export class ByCapitalComponent {
 
   public placeholder: string = 'Enter capital name...';
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountryService) {}
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.countriesService.searchByCapital(term)
       .subscribe(countries => {
         this.countries = countries;
+        this.isLoading = false;
       })  
   }
 }
