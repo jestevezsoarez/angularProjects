32031fa4276d5513dff4ac3c02ab0738eed58c4a
Javier Estevez <javierdaniloestevez@gmail.com>
2024-12-19 03:20:53 -0300

    spinner component creation, and css to style it

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { SearchComponent } from './components/search/search.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 
@@ -16,7 +17,8 @@ import { SearchComponent } from './components/search/search.component';
     AboutComponent,
     SidebarComponent,
     ContactComponent,
-    SearchComponent
+    SearchComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
@@ -26,7 +28,8 @@ import { SearchComponent } from './components/search/search.component';
     HomePageComponent,
     AboutComponent,
     SidebarComponent,
-    SearchComponent
+    SearchComponent,
+    SpinnerComponent
   ]
 })
 export class SharedModule { }
